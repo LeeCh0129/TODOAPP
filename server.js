3b2d926eb8cae57d4ec5544359b594a50d77f653
LeeCh0129 <cksgh5477@gmail.com>
2022-07-31 03:02:53 +0900

    게시물마다 번호 달기 및 DB Update 함수와 inc 연산자

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
 
 var db;
@@ -10,9 +11,10 @@ MongoClient.connect('mongodb+srv://admin:1424125abb!@cluster0.o2erudw.mongodb.ne
     if (에러) return console.log(에러)
     db = client.db('todoapp'); // todoapp 이라는 database(폴더)에 연결
 
-    db.collection('post').insertOne( {이름: 'John', _id : 100}, function(에러, 결과){
-        console.log('저장완료');
-    }); // post라는 파일에 저장
+    // db.collection('post').insertOne( {이름: 'John', _id : 100}, function(에러, 결과){
+    //     console.log('저장완료');
+    // }); 
+    // post라는 파일에 저장
 
     app.listen(8080, function(){
         console.log('listening on 8080')
@@ -44,13 +46,33 @@ app.get('/write',function(req, res){
 
 app.post('/add', function(req, res){
     res.send('전송완료');
-    console.log(req.body.date);
-    console.log(req.body.title);
-    db.collection('post').insertOne( { 제목 : req.body.title, 날짜 : req.body.date}, function(){
-        console.log('저장완료');
+    db.collection('counter').findOne({name : '게시물갯수'}, function(에러, 결과){
+        console.log(결과.totalPost)
+        var 총게시물갯수 = 결과.totalPost;
+        console.log(req.body.date);
+        console.log(req.body.title);
+
+        db.collection('post').insertOne( { _id : 총게시물갯수 + 1, 제목 : req.body.title, 날짜 : req.body.date}, function(){
+            console.log('저장완료');
+            // counter라는 콜렉션에 있는 totalPost 라는 항목도 1 증가시켜야함 (수정);
+            db.collection('counter').updateOne({name:'게시물갯수'}, { $inc : {totalPost:1} }, function(에러, 결과){
+                if(에러){return console.log(에러)}
+            })
+        });
+
     });
+
 });
 
 // /list로 GET요청으로 접속하면
 // 실제 DB에 저장된 데이터들로 예쁘게 꾸며진 HTML을 보여줌
 
+app.get('/list', function(요청, 응답){
+
+    db.collection('post').find().toArray(function(에러, 결과){
+        console.log(결과);
+        응답.render('list.ejs', { posts : 결과 });
+        //디비에 저장된 post라는 collection안의 id가 뭐인 or 제목이 뭐인 or 모든 데이터를 꺼내주세요. find().toArray -> 모든 데이터
+    });
+
+});
