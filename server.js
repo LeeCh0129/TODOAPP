5f624cd41977840d1f9b27b19bbcd6c1f7c7c4ea
LeeCh0129 <cksgh5477@gmail.com>
2022-08-09 21:35:59 +0900

    register function

--- server.js
+++ server.js
@@ -40,17 +40,21 @@ MongoClient.connect('mongodb+srv://admin:1424125abb!@cluster0.o2erudw.mongodb.ne
 
 // 누군가가 /pet 으로 방문을 하면... pet관련된 안내문을 띄워주자
 
-app.get('/beauty',function(요청, 응답){
-    응답.send('뷰티용품 쇼핑할 수 있는 페이지입니다.');
-});
+// app.get('/beauty',function(요청, 응답){
+//     응답.send('뷰티용품 쇼핑할 수 있는 페이지입니다.');
+// });
 
-app.get('/pet',function(요청, 응답){
-    응답.send('펫용품 쇼핑할 수 있는 페이지입니다.');
-});
+// app.get('/pet',function(요청, 응답){
+//     응답.send('펫용품 쇼핑할 수 있는 페이지입니다.');
+// });
 
-app.get('/',function(요청, 응답){
-    응답.sendFile(__dirname + '/index.html');
-});
+// app.get('/',function(요청, 응답){
+//     응답.sendFile(__dirname + 'index.ejs');
+// });
+
+app.get('/', function(요청, 응답){
+    응답.render('index.ejs')
+})
 
 app.get('/write', function(요청, 응답){
     응답.render('write.ejs');
@@ -64,27 +68,7 @@ app.get('/write', function(요청, 응답){
 //     응답.sendFile(__dirname + '/write.html');
 // });
 
-// 어떤 사람이 /add 경로로 POST 요청을 하면... ??를 해주세요~
 
-app.post('/add', function(요청, 응답){
-    응답.send('전송완료');
-    db.collection('counter').findOne({name : '게시물갯수'}, function(에러, 결과){
-        console.log(결과.totalPost)
-        var 총게시물갯수 = 결과.totalPost;
-        // console.log(req.body.date);
-        // console.log(req.body.title);
-
-        db.collection('post').insertOne( { _id : 총게시물갯수 + 1, 제목 : 요청.body.title, 날짜 : 요청.body.date}, function(){
-            console.log('저장완료');
-            // counter라는 콜렉션에 있는 totalPost 라는 항목도 1 증가시켜야함 (수정);
-            db.collection('counter').updateOne({name:'게시물갯수'}, { $inc : {totalPost:1} }, function(에러, 결과){
-                if(에러){return console.log(에러)}
-            })
-        });
-
-    });
-
-});
 
 // /list로 GET요청으로 접속하면
 // 실제 DB에 저장된 데이터들로 예쁘게 꾸며진 HTML을 보여줌
@@ -108,14 +92,7 @@ app.get('/list', function(요청, 응답){
 //     응답.send('삭제완료')
 // });
 
-app.delete('/delete', function(요청, 응답){
-    요청.body._id = parseInt(요청.body._id)
-    db.collection('post').deleteOne(요청.body, function(에러, 결과){
-      console.log('삭제완료');
-      응답.status(200).send({ message : '성공했습니다'});
-    })
 
-  });
 
   // /detail 로 접속하면 detail.ejs 보여줌
   // /detail2로 접속하면 detail2.jes 보여줌
@@ -210,6 +187,60 @@ passport.deserializeUser(function(아이디, done){
     })
     
 }); // 마이페이지 접속시 발동
+
+
+// 회원기능이 필요하면 passport 세팅하는 부분이 위에 있어야함.
+app.post('/register', function(요청, 응답){
+    db.collection('login').insertOne( { id: 요청.body.id, pw : 요청.body.pw }, function(에러, 결과){
+        응답.redirect('/')
+    })
+});
+
+
+// 어떤 사람이 /add 경로로 POST 요청을 하면... ??를 해주세요~
+app.post('/add', function(요청, 응답){
+
+    응답.send('전송완료');
+    db.collection('counter').findOne({name : '게시물갯수'}, function(에러, 결과){
+        console.log(결과.totalPost)
+        var 총게시물갯수 = 결과.totalPost;
+        // console.log(req.body.date);
+        // console.log(req.body.title);
+
+        var 저장할거 = { _id : 총게시물갯수 + 1,  작성자: 요청.user._id, 제목 : 요청.body.title, 날짜 : 요청.body.date }
+
+        db.collection('post').insertOne(저장할거, function(에러, 결과){
+            console.log('저장완료');
+            // counter라는 콜렉션에 있는 totalPost 라는 항목도 1 증가시켜야함 (수정);
+            db.collection('counter').updateOne({name:'게시물갯수'}, { $inc : {totalPost:1} }, function(에러, 결과){
+                if(에러){return console.log(에러)}
+            })
+        });
+
+    });
+
+});
+
+
+app.delete('/delete', function(요청, 응답){
+    console.log('삭제요청');
+    console.log(요청.body);
+    요청.body._id = parseInt(요청.body._id);
+
+    var 삭제할데이터 = { _id : 요청.body._id, 작성자 : 요청.user._id }
+
+    // 요청.body에 담겨온 게시물 번호를 가진 글을 DB에서 찾아서 삭제
+    db.collection('post').deleteOne(삭제할데이터, function(에러, 결과){
+      console.log('삭제완료');
+      if (결과) {console.log(결과)}
+      응답.status(200).send({ message : '성공했습니다'});
+    })
+  });
+
+
+
+
+
 
 app.get('/search', (요청, 응답) => {
     var 검색조건 = [
